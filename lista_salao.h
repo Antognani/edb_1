2bd3f36c1a98419b6b48bf58281f456e1dd01493
guigmats <guilhermeom.2018@gmail.com>
2025-05-26 03:09:16 +0000

    .

--- lista_salao.h
+++ lista_salao.h
@@ -3,10 +3,10 @@
 
 #include "pedido.h"
 
-NoLista* adicionar_pedido_salao(NoLista* cabeca_lista, Pedido novo_pedido);
-NoLista* remover_prato_de_pedido_salao(NoLista* cabeca_lista, int id_pedido_alvo, int id_prato_remover);
-void listar_pedidos_pendentes_salao(NoLista* cabeca_lista);
-void liberar_lista_pedidos_salao(NoLista* cabeca_lista);
-Pedido criar_pedido_interativo(Prato cardapio[], int num_pratos_cardapio, int* proximo_id_pedido);
+NoLista* adicionar_pedido_salao(NoLista* cabecaLista, Pedido novoPedido);
+NoLista* remover_prato_de_pedido_salao(NoLista* cabecaLista, int idPedidoAlvo, int idPratoRemover);
+void listar_pedidos_pendentes_salao(NoLista* cabecaLista);
+void liberar_lista_pedidos_salao(NoLista* cabecaLista);
+Pedido criar_pedido_interativo(Prato cardapio[], int numPratosCardapio, int* proximoIdPedido);
 
 #endif
