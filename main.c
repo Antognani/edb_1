39d783a57ad110fd4b0121b77f49e0951c9fa788
Antonio Gabriel <100813360+Antognani@users.noreply.github.com>
2025-05-26 23:17:57 -0300

    update main.c with extra comments

--- main.c
+++ main.c
@@ -5,6 +5,13 @@
 #include "fila_cozinha.h"
 #include "lista_salao.h"
 
+//PROJETO DESENVOLVIDO POR ANTONIO GABRIEL E GUILHERME MATIAS.
+//TRABALHO 1 DA DISCIPLINA DE ESTRUTURAS DE DADOS BÁSICAS I COM O PROFESSOR EINSTEIN GOMES DOS SANTOS, DO CURSO DE TECNOLOGIA DA INFORMAÇÃO NA UFRN.
+
+//CONVENÇÃO: Utilizar camelCase para nomear variáveis e funções.
+//Sempre comentar partes chave do código, desde que não sejam autoexplicativas ou que já não tenham sido comentadas antes;
+//(por exemplo, é desnecessário recomentar a função limparBufferEntrada, pois ela é autoexplicativa).
+
 #define MAX_PRATOS_CARDAPIO 20 //define o número máximo de pratos do cardápio
 
 Prato cardapio[MAX_PRATOS_CARDAPIO];
